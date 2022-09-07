d622eccfc4f9ca47dcaa6d3ff08d5f56489adf8b
hasnaerguig1999 <112939311+hasnaerguig1999@users.noreply.github.com>
2022-09-07 14:18:48 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	int a;
+	 a=12;
+	 printf("entrer le nombre ");
+	 scanf("%d",&a);
+	 if(a==12){
+	
+	 printf("la condition est valide ");
+	  }
+	  else {
+	  	printf("pas valide");
+	  }
+	  return 0;
+}
